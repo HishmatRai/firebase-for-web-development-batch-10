5ca7e22e21eeb2d0384dab249489fff38772d505
hishmatrai <hishmatrai.2018@gamil.com>
2024-05-30 07:34:28 -0700

    update

--- js/firestore-database.js
+++ js/firestore-database.js
@@ -12,33 +12,69 @@ const addDataHandler = () => {
   //     .catch((error) => {
   //       console.error("Error writing document: ", error);
   //     });
-
   // Add a new document with a generated id.
-
-//   db.collection("users/")
-//     .add({
-//       name: "Testing",
-//       email: "info@gmail.com",
-//     })
-//     .then(() => {
-//       console.log("Document successfully written!");
-//     })
-//     .catch((error) => {
-//       console.error("Error writing document: ", error);
-//     });
-
-
-
-   db.collection("totos/")
-      .doc()
-      .set({
-        name: "Testing",
-        email: "info@gmail.com",
-      })
-      .then(() => {
-        console.log("Document successfully written!");
-      })
-      .catch((error) => {
-        console.error("Error writing document: ", error);
-      });
+  //   db.collection("users/")
+  //     .add({
+  //       name: "Testing",
+  //       email: "info@gmail.com",
+  //     })
+  //     .then(() => {
+  //       console.log("Document successfully written!");
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error writing document: ", error);
+  //     });
+  //  db.collection("totos/")
+  //     .doc()
+  //     .set({
+  //       name: "Testing",
+  //       email: "info@gmail.com",
+  //     })
+  //     .then(() => {
+  //       console.log("Document successfully written!");
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error writing document: ", error);
+  //     });
 };
+
+// get data
+// db.collection("users")
+//   .doc("id2fdfd")
+//   .get()
+//   .then((res) => {
+//     console.log("res", res.data());
+//   })
+//   .catch((err) => {
+//     console.log("error", err);
+//   });
+
+// db.collection("users")
+//   .get()
+//   .then((res) => {
+//     res.forEach((userRes, userIndex) => {
+//       // if(userRes.data().name === "ABC"){
+//         console.log("res--------------", userRes.data());
+
+//       // } 
+//       // if(userRes.data().verified){
+//       //   console.log("verified", userRes.data());
+
+//       // }
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("error", err);
+//   });
+
+
+db.collection("users").where("email", "==", 'abc@gmail.com')
+  .get()
+  .then((res) => {
+    res.forEach((userRes, userIndex) => {
+        console.log("res--------------", userRes.data());
+    });
+  })
+  .catch((err) => {
+    console.log("error", err);
+  });
